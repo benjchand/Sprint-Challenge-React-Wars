dfe232a12cfb1144319490a07b8b2d6009d56fe7
benjchand <benchand@gmail.com>
2019-03-22 09:35:28 -0700

    MVP is completely done.  Questions are answered.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">React Wars CHARACTER LIST</h1>
         
         <Character 
           dataArrayBeingReferenced = {this.state.starwarsChars}
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,7 +4,6 @@ import CharacterElement from './CharacterElement';
 const Character = props => {
     return (
         <div className="character-wrapper">
-            <h1>CHARACTER LIST</h1>
             {props.dataArrayBeingReferenced.map(characterDataBeingExamined =>
                 <CharacterElement 
                     key = {characterDataBeingExamined.created}
